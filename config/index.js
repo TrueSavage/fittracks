c4d473d50fd7c774c0ff8d76701fec01a6acb573
Frank Savage <gotsavaged@gmail.com>
2020-03-15 16:00:02 -0700

    fixed config index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,8 @@
 
-module.exports = require('mongoose').connect('mongodb://heroku_k4kzl5s7:Boltage00!!@ds263107.mlab.com:63107/heroku_k4kzl5s7',
+module.exports = require('mongoose').connect('mongodb://heroku_k4kzl5s7:mmfem488ajeg9uqlf6hgloja8p@ds263107.mlab.com:63107/heroku_k4kzl5s7',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
   });
 
-  
